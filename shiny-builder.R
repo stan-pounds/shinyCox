5d5d365cbadf0084b1703b70a582c135cca79b69
harryc598 <66840256+harryc598@users.noreply.github.com>
2023-06-17 16:23:14 -0400

    Add files via upload

---
+++ shiny-builder.R
@@ -0,0 +1,508 @@
+##### SUBODH EDITED VERSION
+### 2022-04-11: changed shine.coxph ui.code and server.code; added cox predicted times table
+
+
+shine.coxph=function(...)
+  
+{
+  ########################
+  # determine the class of each input argument
+  input.list=list(...)
+  n.list=length(input.list)
+  list.class=rep("",n.list)
+  for (i in 1:n.list)
+    list.class[i]=class(input.list[[i]])
+  
+  #######################
+  # extract the coxph models from the input
+  cox.list=which(list.class=="coxph")
+  n.model=length(cox.list)
+  model.list=vector("list",n.model)
+  for (i in 1:n.model)
+    model.list[[i]]=input.list[[cox.list[i]]]
+  names(model.list)=names(input.list)[cox.list]
+  
+  #######################
+  # get colors list
+  # RColorBrewer brewer.pal
+  clrs=input.list$clrs
+  if (is.null(clrs))
+    clrs=hcl.colors(n.model, "Dark 2", alpha = 1)
+  
+  ##########################
+  # get app directory
+  app.dir=input.list$app.dir
+  if (is.null(app.dir))
+  {
+    app.dir=getwd()
+  }
+  date.time=as.character(Sys.time())
+  date.time=gsub(":","-",date.time,fixed=T)
+  date.time=gsub(" ","-",date.time,fixed=T)
+  app.dir=paste0(app.dir,"/",date.time,"/")
+  dir.create(app.dir)
+  message(paste0("Shiny app will be written in directory ",app.dir,"."))
+  
+  #######################
+  # convert the coxph models to coxfit objects
+  coxfit.list=vector("list",n.model)
+  for (i in 1:n.model)
+    coxfit.list[[i]]=prep.coxfit(model.list[[i]])
+  names(coxfit.list)=names(model.list)
+  
+  #######################
+  # Save app data into app directory
+  cox.fit.list=coxfit.list
+  save(cox.fit.list,clrs,file=paste0(app.dir,"appData.Rdata"))
+  
+  #######################
+  # get different code sections
+  input.data.code=write.coxfit.input.data.code(coxfit.list)
+  KM.plot.code=write.KM.plot.code(coxfit.list,clrs)
+  table.code=prop.haz.tables(cox.fit.list)
+  
+  ########################
+  # generate header code
+  hdr.code=c("#rm(list=ls())",
+             "options(stringsAsFactors=F)",
+             paste0("load('",app.dir,"appData.Rdata')"))
+  
+  
+  ########################
+  # generate ui code
+  ui.code=c("ui=navbarPage(",
+            "             'Cox Model Survival Predictions',",
+            "    tabPanel('Plot',",
+            "             sidebarLayout(",
+            "             sidebarPanel(",
+            paste0("                          ", # SUBODH CHANGED FLOW
+                   input.data.code$ui.code,
+                   ","),
+            "textInput('predProbTimes','Times for predicted probabilities',placeholder='Enter values separated by a comma'),", # SUBODH ADDITION
+            "actionButton(inputId = 'go', label = 'Generate Plot'),",
+            "actionButton(inputId = 'reset', label = 'Reset'),", # SUBODH ADDITION
+            "actionButton(inputId = 'app.exit', label = 'Exit App'),",
+            "),",
+            "mainPanel(",
+            "h3('Predicted Survival Curve'),",
+            "plotOutput(outputId = 'KM'),",
+            "h3('Predicted Probability at Fixed Times'),", # SUBODH ADDITION
+            "textOutput(outputId='noPredTimes'),", # SUBODH ADDITION
+            "tableOutput(outputId = 'cox.times')))),",# SUBODH ADDITION
+            "tabPanel('Summary Tables',",
+            "mainPanel(",
+            # "h3('Hazard Ratio Summary Table'),", # SUBODH ADDITION
+            # "tableOutput(outputId = 'HR'),", # SUBODH ADDITION
+            # "h3('Assessing the Proportional Hazards Assumption'),", # SUBODH ADDITION
+            # "tableOutput(outputId = 'PHA')", # SUBODH ADDITION
+            table.code$ui.code, ### NEW
+            ")))") # SUBODH CHANGED THE FLOW
+  
+  #########################
+  # generate server code
+  server.code=c("server=function(input,output)",
+                "{",
+                "          observeEvent(input$app.exit, {stopApp()}) # Exit when exit button is pressed",
+                "          observeEvent(input$go, {",
+                
+                input.data.code$server.code,
+                KM.plot.code$server.code,
+                table.code$server.code, ### NEW
+                "predProbTable <- cox.times.table(KM.hat,input$predProbTimes)", # SUBODH ADDITION
+                "if (is.null(predProbTable)) output$noPredTimes <- renderText('No input times detected. If you provided times, check that you separated numbers with a single comma and you provided valid numbers.') else output$noPredTimes <- renderText(invisible())", # SUBODH ADDITION
+                "output$cox.times=renderTable(predProbTable,rownames=TRUE)", # SUBODH ADDITION
+                "output$HR=renderTable(cox.fit.list[[1]]$HR.table,rownames=TRUE)", # SUBODH ADDITION
+                "output$PHA=renderTable(cox.fit.list[[1]]$PHA.table$table,rownames=TRUE)", # SUBODH ADDITION
+                "})",
+                "          observeEvent(input$reset, {output$KM <- output$HR <- output$PHA <- output$cox.times <- NULL}) # Reset main area", # SUBODH ADDITION
+                "}") # SUBODH CHANGED THE FLOW
+  
+  ##########################
+  # app code
+  app.code=c("cox.app=shinyApp(ui,server)",
+             "runApp(cox.app)")
+  
+  ###########################
+  # write the app file
+  app.code.file=paste0(app.dir,"shinyCoxapp.R")
+  write(unlist(hdr.code),file=app.code.file,sep="\n")
+  write(unlist(ui.code),file=app.code.file,sep="\n",append=T)
+  write(unlist(server.code),file=app.code.file,sep="\n",append=T)
+  write(unlist(app.code),file=app.code.file,sep="\n",append=T)
+  
+  
+  
+  ##########################
+  # return result
+  
+  res=list(app.dir=app.dir,
+           app.data.file=paste0(app.dir,"/appData.Rdata"),
+           app.code.file=app.code.file,
+           coxfit.list=coxfit.list,
+           hdr.code=hdr.code,
+           ui.code=ui.code,
+           server.code=server.code)
+  
+  return(res)
+  
+}
+
+################################
+# write the shiny code to generate plots
+
+write.KM.plot.code=function(cox.fit.list,clrs)
+{
+  n.models=length(cox.fit.list)
+  ui.code=server.code=NULL
+  
+  ############
+  # server code to initialize KM.hat object
+  
+  initialize.KM.hat=c("n.models=length(cox.fit.list)",
+                      "KM.hat=vector('list',n.models)",
+                      "lp=rep(NA,n.models)",
+                      "names(KM.hat)=names(cox.fit.list)") # here
+  server.code=c(server.code,
+                initialize.KM.hat)
+  
+  ##########
+  # server code to compute KM.hat object
+  
+  compute.KM.hat=c("for (i in 1:n.models)",
+                   "{",
+                   "   km.hat=predict.one.coxfit(cox.fit.list[[i]],new.data)",
+                   "   lp[i]=attr(km.hat,'lp')",
+                   "   sfit=list(time=km.hat$time,surv=km.hat$surv)",
+                   "   class(sfit)='survfit'",
+                   "   KM.hat[[i]]=sfit",
+                   "}")
+  server.code=c(server.code,
+                compute.KM.hat)
+  
+  #########
+  # server and ui code to display KM plots
+  
+  display.KM.server=c("output$KM=renderPlot({cox.KM.plots(KM.hat,clrs=clrs)})")
+  display.KM.ui=c("plotOutput(outputId = 'KM')")
+  
+  ui.code=c(ui.code,
+            display.KM.ui)
+  server.code=c(server.code,
+                display.KM.server)
+  
+  res=list(ui.code=ui.code,
+           server.code=server.code)
+  
+  return(res)
+}
+
+
+#############################
+# Generate Cox predicted KM plots
+
+cox.KM.plots=function(KM.hat,clrs=NULL)
+  
+{
+  n.models=length(KM.hat)
+  if (is.null(clrs))
+    clrs=rainbow(n.models)
+  
+  if (is.null(names(KM.hat)))
+    names(KM.hat)=paste0("model ",1:n.models)
+  
+  max.time=0
+  for (i in 1:n.models)
+    max.time=max(max.time,
+                 max(KM.hat[[i]]$time,na.rm=T))
+  
+  plot(c(0,1.2*max.time),
+       c(0,1),xlab="Time",las=1,
+       ylab="Prob",type="n")
+  
+  for (i in 1:n.models)
+    lines(KM.hat[[i]],col=clrs[i])
+  
+   legend(1.05*max.time,1,
+          col=clrs,lwd=1,
+          legend=names(KM.hat),
+          cex=1)
+}
+
+#############################
+# Generate Cox predicted times table: SUBODH NEW ADDITION
+
+predSurvTime <- function(kmIn,timeIn) { # expects a data frame with columns of time and surv 
+  kmIn$surv[max(which(kmIn$time <= timeIn))]
+}
+
+cox.times.table=function(KM.hat,fixTimes=NULL)
+  
+{
+  n.models=length(KM.hat)
+  
+  if (is.null(names(KM.hat)))
+    names(KM.hat)=paste0("model ",1:n.models)
+  
+  if (is.null(fixTimes) | fixTimes=="") {
+    return(NULL)
+  } else {
+    predTimes <- as.numeric(unlist(strsplit(fixTimes,split=","))) # expects an input character string of numbers each separated by a comma
+    if (any(is.na(predTimes))) return(NULL)
+  }
+  
+  tabOut <- matrix(nrow=n.models,ncol=length(predTimes))
+  rownames(tabOut) <- names(KM.hat)
+  colnames(tabOut) <- paste("Time:",predTimes)
+  
+  for (i in 1:n.models){
+    for (j in 1:length(predTimes)){
+      tabOut[i,j] <- predSurvTime(KM.hat[[i]],predTimes[j])
+    }
+  }
+  return(tabOut)
+}
+
+
+#################################
+# write the shiny code to obtain user inputs
+
+write.coxfit.input.data.code=function(cox.fit.list)
+  
+{
+  
+  if(is.null(names(cox.fit.list)))
+    names(cox.fit.list)=paste0("model ",1:length(cox.fit.list))
+  
+  ###############
+  # Get the set of input variables across all models
+  vnames=get.vnames.cox.fits(cox.fit.list)
+  
+  ############
+  # Get the range of numeric predictor variables
+  num.x.rng.mtx=get.xrng.cox.fits(cox.fit.list,
+                                  vnames)
+  
+  ###########
+  # Get the levels of categorical predictor variables
+  cat.lvls=get.levels.cox.fits(cox.fit.list,
+                               vnames)
+  # NEW
+  #######################################################################
+  # Get levels of logic predictors
+  logic.lvls <- get.logic.cox.fits(cox.fit.list, vnames)
+  ########################################################################
+  #############
+  # Generate shiny code for each variable
+  
+  ui.code=server.code=NULL # initialize shiny code for ui and server
+  
+  if(!is.null(cat.lvls))
+  {
+    for (i in 1:length(cat.lvls))
+    {
+      cat.pick=ez.pickone(names(cat.lvls)[i],
+                          tools::toTitleCase(names(cat.lvls)[i]),
+                          cat.lvls[i])
+      ui.code=c(ui.code,cat.pick$ui.code)
+      server.code=c(server.code,
+                    cat.pick$server.code)
+    }
+  }
+  #NEW
+  ######################################################################
+  if(!is.null(logic.lvls))
+  {
+    for (i in 1:length(logic.lvls))
+    {
+      logic.pick=ez.pickone.logic(names(logic.lvls)[i],
+                          tools::toTitleCase(names(logic.lvls)[i]),
+                          logic.lvls[i])
+      ui.code=c(ui.code,logic.pick$ui.code)
+      server.code=c(server.code,
+                    logic.pick$server.code)
+    }
+  }
+  ##########################################################################
+  
+  if(!is.null(num.x.rng.mtx))
+  {
+    for (i in 1:ncol(num.x.rng.mtx))
+    {
+      x.slider=ez.slider(colnames(num.x.rng.mtx)[i],
+                         colnames(num.x.rng.mtx)[i],
+                         num.x.rng.mtx[1,i],
+                         num.x.rng.mtx[2,i],
+                         mean(num.x.rng.mtx[,i])) # Harrison fixed index
+      ui.code=c(ui.code,x.slider$ui.code)
+      server.code=c(server.code,
+                    x.slider$server.code)
+    }
+  }
+  
+  new.data.code=paste0("new.data = cbind.data.frame(",
+                       paste0(server.code,collapse=","),")")
+  
+  code.res=list(ui.code=ui.code,
+                server.code=new.data.code)
+  
+  return(code.res)
+  
+}
+
+#####################################
+# get the set of unique predictor 
+# variable names
+# from a list of cox.fit objects
+
+get.vnames.cox.fits=function(cox.fit.list)
+{
+  n.models=length(cox.fit.list)
+  var.name=NULL
+  var.type=NULL
+  for (i in 1:n.models) # loop over models
+  {
+    var.name=c(var.name,
+               names(cox.fit.list[[i]]$types))
+    var.type=c(var.type,
+               cox.fit.list[[i]]$types)
+  }
+  dup.name=duplicated(var.name)
+  var.name=var.name[!dup.name]
+  var.type=var.type[!dup.name]
+  # ADDITION
+  # for (i in 1:length(var.type)) {
+  #   var.type[i] <- gsub("logical", "numeric", var.type[i]) #NEW changes logical to numeric
+  # }
+  
+  res=cbind.data.frame(var.name=var.name,
+                       var.type=var.type)
+  
+  return(res)
+}
+
+###############################
+# Get the levels for the categorical variables
+
+get.levels.cox.fits=function(cox.fit.list,vnames)
+{
+  # how to deal with logical
+  cat.vars=which(vnames[,"var.type"]!="numeric" & vnames[,"var.type"]!="logical") 
+  if (length(cat.vars)==0)
+    return(NULL)
+  
+  n.vars=length(cat.vars)
+  n.models=length(cox.fit.list)
+  cat.lvls=vector("list",n.vars)
+  cat.names=vnames[cat.vars,"var.name"]
+  names(cat.lvls)=cat.names
+  
+  for (i in 1:n.models)
+  {
+    mod.lvls=cox.fit.list[[i]]$xlevels
+    mod.vars=names(mod.lvls)
+    for (j in mod.vars)
+    {
+      cat.lvls[[j]]=c(cat.lvls[[j]],
+                      mod.lvls[[j]])
+    }
+  }
+  
+  for (j in 1:length(cat.lvls))
+    cat.lvls[[j]]=unique(cat.lvls[[j]])
+  
+  cat.names=gsub("strata(","",cat.names,fixed=T)
+  cat.names=gsub(")","",cat.names,fixed=T)
+  names(cat.lvls)=cat.names
+  
+  return(cat.lvls)
+  
+}
+
+####################################
+# Get the range of the numeric variables
+# across a list of cox.fit objects
+# Error, rng.mtx[1,x.name]=min(x.rng[1,j],rng.mtx[1,x.name],na.rm=T)
+#        rng.mtx[2,x.name]=min(x.rng[2,j],rng.mtx[2,x.name],na.rm=T) fixed
+get.xrng.cox.fits=function(cox.fit.list,vnames)
+  
+{
+  num.vars=which(vnames[,"var.type"]=="numeric")
+  if (length(num.vars)==0)
+    return(NULL)
+
+  n.models=length(cox.fit.list)
+  rng.mtx=matrix(NA,2,length(num.vars))
+  rng.mtx[1,]=NA
+  rng.mtx[2,]=NA
+  colnames(rng.mtx)=vnames[num.vars,"var.name"]
+  
+  
+  for (i in 1:n.models)
+  {
+    # ADDITION by harry
+    #colnames(cox.fit.list[[i]]$num.x.rng) <- gsub("TRUE", "", colnames(cox.fit.list[[i]]$num.x.rng))
+    x.rng=cox.fit.list[[i]]$num.x.rng
+    for (j in 1:ncol(rng.mtx))
+    {
+      x.name=colnames(rng.mtx)[j]
+      rng.mtx[1,x.name]=min(x.rng[1,x.name],rng.mtx[1,x.name],na.rm=T)
+      rng.mtx[2,x.name]=max(x.rng[2,x.name],rng.mtx[2,x.name],na.rm=T) #changed from min()
+    }
+  }
+  return(rng.mtx)
+}
+
+
+# add tab with plot hazard ratio table
+# NEW function
+########################################
+get.logic.cox.fits <- function(cox.fit.list, vnames) {
+  logic.vars=which(vnames[,"var.type"]=="logical")
+  if (length(logic.vars)==0)
+    return(NULL)
+
+  n.vars <- length(logic.vars)
+  n.models <- length(cox.fit.list)
+  logic.levels <- vector("list", n.vars)
+  logic.names <- vnames[logic.vars, "var.name"]
+  names(logic.levels) <- logic.names
+
+  for (i in 1:n.vars) {
+    logic.levels[[i]] <- c("TRUE", "FALSE")
+  }
+
+  return(logic.levels)
+}
+
+# NEW function, proportional hazard for each
+##################################################################
+prop.haz.tables <- function(cox.fit.list) {
+  ui.code=c("tabsetPanel(")
+  server.code=c()
+  for (i in 1:length(cox.fit.list)) {
+    
+    server.code = c(server.code,paste0("output$HR", i, "=renderTable(cox.fit.list[[",i, "]]$HR.table,rownames=TRUE)"),
+                    paste0("output$PHA", i, "=renderTable(cox.fit.list[[",i, "]]$PHA.table$table,rownames=TRUE)"),
+                    paste0("output$nevents", i, "=renderText(paste('Number of events: ',cox.fit.list[[",i, "]]$nevents))"))
+    
+    if(i < length(cox.fit.list)){
+    ui.code = c(ui.code,paste0("tabPanel('",names(cox.fit.list)[i],"',"),
+                "'Hazard Ratio Summary Table',",
+                paste0("column(12, align = 'center', tableOutput(outputId = 'HR", i, "')),"),
+                "h4('Assesing the Proportional Hazards Assumption'),",
+                paste0("column(12, align = 'center', tableOutput(outputId = 'PHA", i, "')),"),
+                paste0("textOutput(outputId = 'nevents", i, "')"),
+                "),") } else
+                  ui.code = c(ui.code,paste0("tabPanel('",names(cox.fit.list)[i],"',"),
+                              "'Hazard Ratio Summary Table',",
+                              paste0("column(12, align = 'center', tableOutput(outputId = 'HR", i, "')),"),
+                              "h4('Assesing the Proportional Hazards Assumption'),",
+                              paste0("column(12, align = 'center', tableOutput(outputId = 'PHA", i, "')),"),
+                              paste0("textOutput(outputId = 'nevents", i, "')"),
+                              ")")
+  }
+  ui.code=c(ui.code, ")")
+  code.res=list(ui.code=ui.code,
+                server.code=server.code)
+  return(code.res)
+}
---
+++ test-shiny-Cox.R
@@ -0,0 +1,221 @@
+#######################################
+# Initial set-up
+
+rm(list=ls()); options(stringsAsFactors=F)
+library(survival)
+
+
+##################################
+# Generate an example data set
+#
+set.seed(2023)
+n=500
+x1=rnorm(n)              # predictor x
+u1=runif(n)              # predictor u
+v1=rexp(n)               # predictor v
+tm1=rexp(n)                  # event time
+ev1=rbinom(n,1,0.5)          # event indicator
+srv1=Surv(tm1,ev1)
+grp1=sample(LETTERS[1:3],n,T)
+sex1=sample(c("male","female"),n,T)
+strta=rep(1:2,n/2)
+strt2a=rep(LETTERS[1:2],each=n/2)
+
+
+dset1=cbind.data.frame(grp=grp1,x=x1,
+                      u=u1,v=v1,
+                      sex=sex1,
+                      ev=ev1,tm=tm1,
+                      strt=strta,
+                      srv=srv1)
+
+
+x2=rnorm(n)              # predictor x
+u2=runif(n)              # predictor u
+v2=rexp(n)               # predictor v
+tm2=rexp(n)                  # event time
+ev2=rbinom(n,1,0.5)          # event indicator
+srv2=Surv(tm2,ev2)
+grp2=sample(LETTERS[1:3],n,T)
+sex2=sample(c("male","female"),n,T)
+strtb=rep(1:2,n/2)
+strt2b=rep(LETTERS[1:2],each=n/2)
+
+
+dset2=cbind.data.frame(grp=grp2,x=x2,
+                       u=u2,v=v2,
+                       sex=sex2,
+                       ev=ev2,tm=tm2,
+                       strt=strtb,
+                       srv=srv2)
+
+x3=rnorm(n)              # predictor x
+u3=runif(n)              # predictor u
+v3=rexp(n)               # predictor v
+tm3=rexp(n)                  # event time
+ev3=rbinom(n,1,0.5)          # event indicator
+srv3=Surv(tm3,ev3)
+grp3=sample(LETTERS[1:3],n,T)
+sex3=sample(c("male","female"),n,T)
+strtc=rep(1:2,n/2)
+strt2c=rep(LETTERS[1:2],each=n/2)
+
+
+dset3=cbind.data.frame(grp=grp3,x=x3,
+                       u=u3,v=v3,
+                       sex=sex3,
+                       ev=ev3,tm=tm3,
+                       strt=strtc,
+                       srv=srv3)
+
+x4=rnorm(n)              # predictor x
+u4=runif(n)              # predictor u
+v4=rexp(n)               # predictor v
+tm4=rexp(n)                  # event time
+ev4=rbinom(n,1,0.5)          # event indicator
+srv4=Surv(tm4,ev4)
+grp4=sample(LETTERS[1:3],n,T)
+sex4=sample(c("male","female"),n,T)
+strtd=rep(1:2,n/2)
+strt2d=rep(LETTERS[1:2],each=n/2)
+
+
+dset4=cbind.data.frame(grp=grp4,x=x4,
+                       u=u4,v=v4,
+                       sex=sex4,
+                       ev=ev4,tm=tm4,
+                       strt=strtd,
+                       srv=srv4)
+##########################################
+# Fit a cox model to the example data set
+
+cox.fit1=coxph(srv~sex+log(u)+v+grp+strata(strt),
+               data=dset1,
+               x=TRUE,model=TRUE)
+
+cox.fit2=coxph(srv~sex+log(u)+v+grp+strata(strt),
+               data=dset2,
+               x=TRUE,model=TRUE)
+
+cox.fit3=coxph(srv~sex+log(u)+v+grp+strata(strt),
+               data=dset3,
+               x=TRUE,model=TRUE)
+
+cox.fit4=coxph(srv~sex+log(u)+v+grp+strata(strt),
+               data=dset4,
+               x=TRUE,model=TRUE)
+########################################
+# Remove example data set to ensure no inadvertent access to it while building app
+
+# rm(dset1)
+# rm(dset2)
+# rm(dset3)
+# rm(dset4)
+
+########################################
+# Generate an app for the example data set model
+
+source("~/Project/ShinyCox/cox-predictions.R")
+source("~/Project/ShinyCox/shiny-blocks.R")
+source("~/Project/ShinyCox/shiny-builder.R")
+app.dir="~/Project/ShinyCox/TestApps/"
+
+test.res=shine.coxph(fit1 = cox.fit1,fit2 = cox.fit2,
+                     fit3 = cox.fit3, fit4 = cox.fit4,# pass the coxph fit result
+                     app.dir=app.dir,  # application directory
+                     launch.app=T)     # indicates whether to launch the app after it is created
+
+
+
+# REAL DATA EXAMPLE
+##################################################################################
+# library(magrittr)
+# setwd("C:/Users/hclement/Documents/Project/ShinyCox")
+# datset <- readxl::read_xlsx("AML08-IDs-clinical-2021-06-06-for-JL.xlsx")
+# 
+# dataset <- subset(datset, datset$randarm != 9)
+# df2 <- split(dataset, dataset$randarm)
+# arm1 <- df2$`1`
+# arm2 <- df2$`2`
+# 
+# # predictors age, leukocyte count at diagnosis, MRD, FLT3-ITD, CBF leukemia,
+# # KMT2A fusions, presence of the M0, M6, or M7 FAB types
+# 
+# srv1 <- Surv(arm1$efstime, arm1$efscensor)
+# srv2 <- Surv(arm2$efstime, arm2$efscensor)
+# 
+# arm1[arm1 == "N/A"] <- NA
+# arm2[arm2 == "N/A"] <- NA
+# 
+# arm1 <- arm1 %>%
+#   dplyr::mutate(M0.M6.M7 = (FAB == "M0" | FAB == "M6" | FAB == "M7"),
+#          KMT2A = (grepl("11q23", arm1$PRIMARYDIAGNOSIS)),
+#          Age.over.10 = (ONSTUDYDATE/365.25 >= 10),
+#          MRDpos = (mrdgrp1 == 2),
+#          FLT3ITD = (FLT3 == "ITD"),
+#          WBCover50 = (WBC >= 50))
+# arm2 <- arm2 %>%
+#   dplyr::mutate(M0.M6.M7 = (FAB == "M0" | FAB == "M6" | FAB == "M7"),
+#                 KMT2A = (grepl("11q23", arm2$PRIMARYDIAGNOSIS)),
+#                 Age.over.10 = (ONSTUDYDATE/365.25 >= 10),
+#                 MRDpos = (mrdgrp1 == 2),
+#                 FLT3ITD = (FLT3 == "ITD"),
+#                 WBCover50 = (WBC >= 50))
+# 
+# arm1.fit <- coxph(srv1 ~ WBCover50 + M0.M6.M7 + CBF +
+#                     KMT2A + Age.over.10 + MRDpos, arm1, x = TRUE, model = TRUE)
+# 
+# arm2.fit <- coxph(srv2 ~ WBCover50 + M0.M6.M7 + CBF +
+#                     KMT2A + Age.over.10 + MRDpos, arm2, x = TRUE, model = TRUE)
+# #
+# # rm(arm1)
+# # rm(arm2)
+# 
+# test.real <- shine.coxph(HDADE = arm1.fit,CloArac = arm2.fit,
+#                          app.dir = app.dir)
+# 
+
+##############################################
+# Now try for the AML02 data set
+
+
+# aml08.arm1 <- read.csv("arm1.csv")
+# 
+# NEED TO EDIT
+# 
+# 
+# aml02.cox.efs=coxph(EFS~WBC+AGE+initialRisk,data=aml02.data,
+#                     x=T,model=T)
+# aml02.shiny=shine.coxph(aml02.cox.efs,
+#                         app.dir=app.dir,
+#                         launch.app=T)
+
+
+
+######################################################
+
+# srv <- Surv(bladder$stop, bladder$event)
+# rx1 <- as.factor(bladder$rx)
+# 
+# bladder.ph <- coxph(srv ~ rx1 + number + size, bladder, x = TRUE, 
+#                     model = TRUE)
+# 
+# bladde.res <- shine.coxph(bladder.ph,
+#                           app.dir = app.dir)
+
+# 
+# amltable <- data.frame(time = aml$time, status = aml$status, ex = aml$x,
+#                        srv = Surv(aml$time, aml$status))
+# 
+# fit <- coxph(srv ~ ex, amltable, x = TRUE, model = TRUE)
+# 
+# aml.test <- shine.coxph(fit, app.dir=app.dir)
+
+
+# 
+# srv <- Surv(cgd$tstart, cgd$tstop, cgd$status)
+# 
+# cgd.fit <- coxph(srv ~ treat + sex + age + inherit, cgd, x = TRUE, model = TRUE)
+# 
+# test.cgd <- shine.coxph(only = cgd.fit, app.dir = app.dir)
+
